81a6e8bbd36f498a0683603af9ea94bfd7a2348c
Dhanushka Madhushanka <dhanushkamg1234@gmail.com>
2026-09-05 06:43:17 +0530

    Update certifications

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -1,13 +1,18 @@
 export const certifications = [
   {
-    title: "Java (Basic)",
-    platform: "HackerRank",
-    url: null,
+    title: "AWS Cloud Practitioner Essentials",
+    platform: "AWS",
+    url: "https://drive.google.com/file/d/1ll5fqVM9uq1_JVO5hOlpA4MDYn-qWml_/view?usp=sharing",
   },
   {
-    title: "Frontend Developer (React)",
-    platform: "HackerRank",
-    url: null,
+    title: "CSS, JavaScript And Python Complete Course",
+    platform: "Udemy",
+    url: "https://www.udemy.com/certificate/UC-aa0f52b0-f867-42d8-a7a5-b5fb68311632/",
+  },
+  {
+    title: "PHP with MySQL",
+    platform: "Udemy",
+    url: "https://www.udemy.com/certificate/UC-00a70759-5eca-4800-b873-bc2d19966c57/",
   },
   {
     title: "JavaScript From Scratch",
@@ -18,10 +23,5 @@ export const certifications = [
     title: "Mastering React 18",
     platform: "Udemy",
     url: "https://www.udemy.com/certificate/UC-90d89c68-fafa-4fe9-a1ce-83cd3fc5db7c/",
-  },
-  {
-    title: "Mastering AI Agents Bootcamp",
-    platform: "Udemy",
-    url: null,
   }
 ];
