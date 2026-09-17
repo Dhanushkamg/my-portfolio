5a86a31b4137c6e14f9879a476a6716748216c62
Dhanushka Madhushanka <dhanushkamg1234@gmail.com>
2026-09-17 21:21:23 +0530

    feat(data): add new designer roles to extracurriculars

--- src/data/leadership.ts
+++ src/data/leadership.ts
@@ -3,5 +3,15 @@ export const leadership = [
     role: "Batch Representative",
     organization: "Faculty of Information Technology",
     description: "Representing the IT undergraduate batch, coordinating academic matters, and bridging communication between students and faculty."
+  },
+  {
+    role: "Designer",
+    organization: "IEEE RAS Student Branch",
+    description: ""
+  },
+  {
+    role: "Designer",
+    organization: "oGT Marcom, AIESEC",
+    description: ""
   }
 ];
